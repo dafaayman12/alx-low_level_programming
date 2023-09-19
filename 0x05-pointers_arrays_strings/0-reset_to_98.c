7fae7c5b74df1150f3c32a3841f723d67f86ae16
dafaaymam12 <dafaayman@gmail.com>
2023-09-19 03:58:28 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int parameter and updfates the value if points to 98
+ *
+ * @n: function parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
