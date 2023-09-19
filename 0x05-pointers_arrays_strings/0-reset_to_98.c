6d0990b34fc8c598f42b4fe42553452dec9df539
dafaaymam12 <dafaayman@gmail.com>
2023-09-19 04:07:28 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int parameter and updfates the value if points to 98
+ * reset_to_98 - takes a pointer to an int parameter
+ * and updfates the value if points to 98
  *
  * @n: function parameter
  *
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef: MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -15,4 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
