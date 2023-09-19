02da8c1087ffe7be341bcdc37cd9b89aebd4619a
dafaaymam12 <dafaayman@gmail.com>
2023-09-19 05:03:06 -0700

    e

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - the function name
+ * @s: function parameter
+ */
+
+void print_rev(char *s)
+
+{
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+count++;
+
+for (i = count - i; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
