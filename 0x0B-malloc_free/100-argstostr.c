96fa9d113f1291044199a6ddb747f33928743079
dafaaymam12 <dafaayman@gmail.com>
2023-10-05 07:20:29 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,15 +24,15 @@ return (size);
 
 char *argstostr(int ac, char **av)
 {
-int i = 0, j = 0, cmpt = 0;
+int i = 0, nc = 0, j = 0, cmpt = 0;
 char *s;
 if (ac == 0 || av == NULL)
 return (NULL);
 
-for (; i < ac; i++, ac++)
-ac += _strlen(av[i]);
+for (; i < ac; i++, nc++)
+nc += _strlen(av[i]);
 
-s = malloc(sizeof(char) * ac + 1);
+s = malloc(sizeof(char) * nc + 1);
 if (s == 0)
 return (NULL);
 
